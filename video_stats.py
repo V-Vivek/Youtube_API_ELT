fda13ad7d420adc7c6c050cd5bdf848cf662f01a
V-Vivek <jadhav.vivek.18.3@gmail.com>
2026-06-24 11:38:05 +0530

    Added video_stats

---
+++ video_stats.py
@@ -0,0 +1,24 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='./.env')
+API_KEY = os.getenv('API_KEY')
+CHANNEL_HANDLE = 'MrBeast'
+# CHANNEL_HANDLE = 'TechWorldwithNana'
+
+def get_playlist_id():
+    try:
+        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()
+
+        channel_items = data["items"][0]
+        channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        return channel_playlist_id
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == "__main__":
+    print(get_playlist_id())
